1cd59c395d720475022494b053b855ce4dd53e6d
이방환 <66163043+hmschlng@users.noreply.github.com>
2024-02-15 10:34:20 +0900

    Update MemberController.java

--- backend/src/main/java/com/ssafy/backtest/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/backtest/member/controller/MemberController.java
@@ -20,7 +20,7 @@ import com.ssafy.backtest.member.model.dto.Member;
 
 @RestController
 @CrossOrigin
-@RequestMapping("member")
+@RequestMapping("/api/member")
 public class MemberController {
 	
 	@Autowired
