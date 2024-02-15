7e10bcadbf3d3e9b80d7d8a8be634a8bf3bc290d
이방환 <66163043+hmschlng@users.noreply.github.com>
2024-02-15 09:08:58 +0900

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function apiInstance() {
   const instance = axios.create({
-    baseURL: "https://key-board.o-r.kr:8080",
+    baseURL: "https://key-board.o-r.kr",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
